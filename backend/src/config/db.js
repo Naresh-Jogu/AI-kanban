ca8abaecf1bcecedae5938245df2aea8d4b5547b
Naresh Jogu <kumarn29346@gmail.com>
2026-07-16 21:58:51 +0530

    postegreSQL database connection

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,19 +1,24 @@
-const { pool } = require("pg");
+// Pool class from postgresql library pg
+const { Pool } = require("pg");
 
+// database connection
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
-    ssl: {rejectUnauthorized: false},
+    ssl: {rejectUnauthorized: false}, // ssl => Secure socket layer
     max: 10,
     idleTimeoutMillis: 30000,
     connectionTimeoutMillis: 10000,
 });
 
+// shows error when db not connected
 pool.on("error", (err) => {
     console.error("Unexpected postgreSQL pool error:", err)
 });
 
+// query is helper function 
 const query = (text, params) => pool.query(text, params);
 
+// mutli query async helper function
 const withTransaction = async (callback) => {
     const client = await pool.connect();
     try {
@@ -21,7 +26,7 @@ const withTransaction = async (callback) => {
 
         const result = await callback(client);
 
-        await client.query("CONNECT");
+        await client.query("COMMIT");
 
         return result
     } catch (err) {
