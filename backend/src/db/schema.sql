5f2fb09b41b3df985b83e377bf628a02211af0f0
Naresh Jogu <kumarn29346@gmail.com>
2026-07-17 16:51:51 +0530

    schema updated to neon database server

--- backend/src/db/schema.sql
+++ backend/src/db/schema.sql
@@ -1,6 +1,6 @@
-CREATE EXTENTION IF NOT EXISTS "pgcrypto" 
+CREATE EXTENSION IF NOT EXISTS "pgcrypto";
 
-CREATE TABLE IF NOT EXISTS users (
+CREATE TABLE IF NOT EXISTS users(
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     name TEXT NOT NULL,
     email TEXT NOT NULL UNIQUE,
@@ -22,7 +22,7 @@ CREATE TABLE IF NOT EXISTS boards(
 
 );
 
-CREATE TABLE IF NOT EXISTS board_members (
+CREATE TABLE IF NOT EXISTS board_members(
     board_id UUID NOT NULL REFERENCES boards(id) ON DELETE CASCADE,
     user_id UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE,
     role TEXT NOT NULL DEFAULT 'member',
@@ -32,27 +32,27 @@ CREATE TABLE IF NOT EXISTS board_members (
 
 );
 
-CREATE TABLE IF NOT EXISTS columns (
+CREATE TABLE IF NOT EXISTS columns(
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     board_id UUID NOT NULL REFERENCES boards(id) ON DELETE CASCADE,
     title TEXT NOT NULL,
     position DOUBLE PRECISION NOT NULL DEFAULT 1000,
     created_at TIMESTAMPTZ NOT NULL DEFAULT now()
-)
+);
 
 
 
-CREATE TABLE IF NOT EXISTS tasks (
+CREATE TABLE IF NOT EXISTS tasks(
      id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
      board_id UUID NOT NULL REFERENCES boards(id) ON DELETE CASCADE,
-     coumn_id UUID NOT NULL REFERENCES column(id) ON DELETE CASCADE,
-     tilte TEXT NOT NULL,
+     column_id UUID NOT NULL REFERENCES columns(id) ON DELETE CASCADE,
+     title TEXT NOT NULL,
      description TEXT,
      priority TEXT NOT NULL DEFAULT 'medium',
      due_date TIMESTAMPTZ,
-     assignee_id UUID REFERENCES user(id) ON DELETE SET NULL,
+     assignee_id UUID REFERENCES users(id) ON DELETE SET NULL,
      position DOUBLE PRECISION  NOT NULL DEFAULT 1000,
-     created_by UUID REFERENCES user(id) ON DELETE SET NULL,
+     created_by UUID REFERENCES users(id) ON DELETE SET NULL,
      created_at TIMESTAMPTZ NOT NULL DEFAULT now(),
      updated_at TIMESTAMPTZ NOT NULL DEFAULT now()
 
@@ -61,23 +61,23 @@ CREATE TABLE IF NOT EXISTS tasks (
 );
 
 
-CREATE TABLE IF NOT EXISTS activities (
+CREATE TABLE IF NOT EXISTS activities(
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    board_id UUID NOT NULL REFERENCES board(id) ON DELETE CASCADE,
-    user_id UUID NOT NULL REFERENCES user(id) ON DELETE SET NULL,
+    board_id UUID NOT NULL REFERENCES boards(id) ON DELETE CASCADE,
+    user_id UUID REFERENCES users(id) ON DELETE SET NULL,
     action TEXT NOT NULL,
     message TEXT NOT NULL,
     metadata JSONB,
-    created_at TIMESTAMPTZ NOT NULL DEFAULT now(),
+    created_at TIMESTAMPTZ NOT NULL DEFAULT now()
 
 );
 
-CREATE INDEX IF NOT EXISTS idx_boards_owner     ON boards(owner_id),
-CREATE INDEX IF NOT EXISTS idx_columns_board    ON columns(board_id),
-CREATE INDEX IF NOT EXISTS idx_tasks_board      ON tasks(board_id),
-CREATE INDEX IF NOT EXISTS idx_tasks_column     ON tasks(column_id),
-CREATE INDEX IF NOT EXISTS idx_tasks_asignee    ON tasks(asignee_id),
-CREATE INDEX IF NOT EXISTS idx_members_user     ON baord(user_id),
-CREATE INDEX IF NOT EXISTS idx_activities_board ON activities(baord_id, created_at DESC),
+CREATE INDEX IF NOT EXISTS idx_boards_owner     ON boards(owner_id);
+CREATE INDEX IF NOT EXISTS idx_columns_board    ON columns(board_id);
+CREATE INDEX IF NOT EXISTS idx_tasks_board      ON tasks(board_id);
+CREATE INDEX IF NOT EXISTS idx_tasks_column     ON tasks(column_id);
+CREATE INDEX IF NOT EXISTS idx_tasks_assignee    ON tasks(assignee_id);
+CREATE INDEX IF NOT EXISTS idx_members_user     ON board_members(user_id);
+CREATE INDEX IF NOT EXISTS idx_activities_board ON activities(board_id, created_at DESC);
 
 
