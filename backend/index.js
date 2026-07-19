be487a50c56963767ec29939e30c3fa876cf4f4b
Arun Kumar <akulakumar453@gmail.com>
2026-07-19 20:10:55 +0530

    updated port value

--- backend/index.js
+++ backend/index.js
@@ -28,7 +28,7 @@ app.use("/api", apiRoutes);
 app.use(notFoundHandler);
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 5001 
+const PORT = process.env.PORT || 5000 
 app.listen(PORT, () => {
     console.log(`API Listening on http://localhost:${PORT}`);
 })
