8135e2ac845cf9cb94100c0ed7a234a8041a674d
Arun Kumar <akulakumar453@gmail.com>
2026-07-19 20:15:32 +0530

    middleware applied for routes

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const authRoutes = require("./authRoutes");
 
 
 const router = express.Router();
@@ -7,5 +8,7 @@ const router = express.Router();
 router.get("/health", (_req, res) => res.json({
     status: "ok",
 }));
+
+router.use("/auth", authRoutes);
 
 module.exports = router;
