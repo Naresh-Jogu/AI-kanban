0167dc5b651aa5c8e47be4f553a3628e8f715c5a
Arun Kumar <akulakumar453@gmail.com>
2026-07-19 17:33:50 +0530

    create jwt token and verifyed that token

--- backend/src/utils/jwt.js
+++ backend/src/utils/jwt.js
@@ -1,2 +1,11 @@
 const jwt = require("jsonwebtoken")
 
+
+// generating JWT token
+const signToken = (playLoad) => jwt.sign(playLoad, process.env.JWT_SECRET, {expiresIn: process.env.JWT_EXPIRES || "7d"})
+
+// verifying JWT token 
+const verifyToken = (token) => jwt.verify(token, process.env.JWT_SECRET);
+
+
+module.exports = {signToken, verifyToken};
