7cafbb3810fc69c3c77e46e9eba5ea59d392c0b1
Naresh Jogu <kumarn29346@gmail.com>
2026-07-20 16:56:49 +0530

    router crud method missmatched updated from post to get

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -7,7 +7,7 @@ const router = express.Router();
 
 router.post("/register", register);
 router.post("/login", login);
-router.post("/me", requireAuth, me);
+router.get("/me", requireAuth, me);
 
 
 module.exports = router;  
